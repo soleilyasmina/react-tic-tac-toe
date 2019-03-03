96b1c0571426b3580e63153b9361193b0686cc10
soleilyasmina <mounlasolomon@gmail.com>
2019-03-03 17:59:51 -0500

    rendered one Space in Board

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,8 +3,8 @@ import Space from './Space';
 
 export default function Board(props) {
   return (
-    <div>
-      <h1>HELLO</h1>
+    <div className="Board">
+      <Space />
     </div>
   )
 }
---
+++ src/components/Space.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Space(props) {
+  return (
+    <div>
+      <h1>{ props.index }</h1>
+    </div>
+  )
+}
