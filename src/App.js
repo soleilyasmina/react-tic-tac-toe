c4f9dca3b136f64d87ed5a41c12d97cede20e376
soleilyasmina <mounlasolomon@gmail.com>
2019-03-03 17:53:56 -0500

    implemented Board with no props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Board from './components/Board';
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Board />
       </div>
     );
   }
