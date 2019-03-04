57e669cf41822d4ad2f4ba55d283c2b0321964b3
soleilyasmina <mounlasolomon@gmail.com>
2019-03-03 21:53:42 -0500

    first functional back end edit - added resetGame, and awardPoints to fulfill those needs - added reset in game so players cannot play while the game resets - implemented win conditions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Board from './components/Board';
+import win from './services/winconditions';
 
 class App extends Component {
   constructor(props) {
@@ -11,21 +12,49 @@ class App extends Component {
         null, null, null,
         null, null, null
       ],
-      turn: 'X'
+      turn: 'X',
+      points: [0,0],
+      reset: false
     }
     this.changeTurn = this.changeTurn.bind(this);
+    this.resetGame = this.resetGame.bind(this);
     this.updateBoard = this.updateBoard.bind(this);
   }
-  changeTurn = () => this.state.turn === 'X' ? this.setState({ turn: 'O' }) : this.setState({ turn: 'X' });
 
-  updateBoard = index => {
-    let { board, turn } = this.state;
-    if (board[index]) return;
-    board[index] = turn;
-    this.setState({ board });
-    this.changeTurn();
+  changeTurn = async () => await this.state.turn === 'X' ? this.setState({ turn: 'O' }) : this.setState({ turn: 'X' });
+
+  async awardPoints() {
+    let { points, turn } = await this.state;
+    turn === 'X' ? points[0] += 1 : points[1] += 1;
+    await this.setState({ points });
   }
-  render = () => {
+
+  async resetGame() {
+    await this.setState({ reset: true });
+    await this.awardPoints();
+    await setTimeout(() => {
+      this.setState({ 
+        board: [
+          null, null, null,
+          null, null, null,
+          null, null, null
+        ],
+        turn: 'X',
+        reset: false
+      });
+    }, 1500);
+  }
+  
+  async updateBoard(index) {
+    let { board, turn, reset } = await this.state;
+    if (board[index] !== null || reset === true) return;
+    board[index] = turn;
+    await this.setState({ board });
+    win(board) ? 
+    await this.resetGame() :
+    await this.changeTurn();
+  }
+  render() {
     return (
       <div className="App">
         <Board board={this.state.board} update={this.updateBoard}/>
