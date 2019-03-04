f562797f4cb6d968654c265ed916bcc0013ea3f9
soleilyasmina <mounlasolomon@gmail.com>
2019-03-03 19:33:30 -0500

    added state, update, and updated board with props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 import Board from './components/Board';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: [
+        null, null, null,
+        null, null, null,
+        null, null, null
+      ],
+      turn: 'X'
+    }
+    this.updateBoard = this.updateBoard.bind(this);
+  }
+  updateBoard(index) {
+    let { board, turn } = this.state;
+    board[index] = turn;
+    this.setState({ board });
+  }
   render() {
     return (
       <div className="App">
-        <Board />
+        <Board board={this.state.board} update={this.updateBoard}/>
       </div>
     );
   }
