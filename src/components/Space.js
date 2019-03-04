55b1c622d3ad7408282a92e56ed9a54e87268372
soleilyasmina <mounlasolomon@gmail.com>
2019-03-03 23:10:45 -0500

    added className and changed h1 to h2

--- src/components/Space.js
+++ src/components/Space.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Space(props) {
   return (
-    <div onClick={() => props.update(props.index)}>
-      <h1>{ props.index }</h1>
+    <div className="Space" onClick={() => props.update(props.index)}>
+      <h2>{ props.space }</h2>
     </div>
   )
 }
